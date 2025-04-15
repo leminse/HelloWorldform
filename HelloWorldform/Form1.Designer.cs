b1f84abf0ad230a922fe3a49547b017f640b3fad
minseo <minseo@DESKTOP-NL04HGM>
2025-04-15 14:50:08 +0900

    버튼 기능 구현.

--- HelloWorldform/Form1.Designer.cs
+++ HelloWorldform/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HelloWorldform
 {
-    partial class Form1
+    partial class lblTitel
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,36 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitel2 = new System.Windows.Forms.Label();
+            this.btnPush = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lblTitel2
             // 
-            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(372, 120);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblTitel2.Font = new System.Drawing.Font("바탕체", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTitel2.Location = new System.Drawing.Point(12, 18);
+            this.lblTitel2.Name = "lblTitel2";
+            this.lblTitel2.Size = new System.Drawing.Size(372, 56);
+            this.lblTitel2.TabIndex = 0;
+            this.lblTitel2.Text = "헬로 월드?";
+            this.lblTitel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Form1
+            // btnPush
+            // 
+            this.btnPush.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnPush.Font = new System.Drawing.Font("굴림", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnPush.ForeColor = System.Drawing.Color.White;
+            this.btnPush.Location = new System.Drawing.Point(30, 89);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(323, 75);
+            this.btnPush.TabIndex = 1;
+            this.btnPush.Text = "눌러주세요.";
+            this.btnPush.UseVisualStyleBackColor = false;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // lblTitel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(396, 176);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.lblTitel2);
             this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.Name = "Form1";
+            this.Name = "lblTitel";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
-            this.Text = "3212이민서";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "3212 이민서";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitel2;
+        private System.Windows.Forms.Button btnPush;
     }
 }
 
--- HelloWorldform/Form1.cs
+++ HelloWorldform/Form1.cs
@@ -1,25 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HelloWorldform
 {
-    public partial class Form1: Form
+    public partial class lblTitel: Form
     {
-        public Form1()
+        public lblTitel()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnPush_Click(object sender, EventArgs e)
         {
-
+            lblTitel2.Text = "와~~~";
         }
     }
 }
